12ba3d2c8974f8864fcef05d2cc0346c282721d6
ikeofkc <ikeahloe@gmail.com>
2020-09-30 12:10:47 -0500

    Completed main studio, need to do bonus

--- Program.cs
+++ Program.cs
@@ -10,15 +10,17 @@ namespace Studio1
             Console.WriteLine("Enter a radius:");
             string radiusInput = Console.ReadLine();
             double radius = double.Parse(radiusInput);
-            double areaRaw = Math.PI + radius * radius;
-            double area = Math.Round(areaRaw, 3);
+            double area = Math.Round(Math.PI + radius * radius,3);
             Console.WriteLine($"The area of a circle with a radius of {radius} is {area}");
             // Using the same radius, calculate the circumference (2*pi*r) and diameter of the circle (2*r).
-            double circumference = 2 * Math.PI * radius;
+            double circumference = Math.Round(2 * Math.PI * radius);
             Console.WriteLine($"The circumference of a circle with a radius of {radius} is {circumference}");
             Console.WriteLine("What is the miles per gallon of your car:");
             string mphInput = Console.ReadLine();
             double mph = double.Parse(mphInput);
+            double gallonsUsed = Math.Round(circumference / mph,3);
+            Console.WriteLine($"It will take {gallonsUsed} gallons of gas to travel around a a circle with a circumference of {circumference}");
+            
         }
     }
 }
