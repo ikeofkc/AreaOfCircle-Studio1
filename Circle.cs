441f1b5020fba6962b4f0e5e46f0a8106bb80315
ikeofkc <ikeahloe@gmail.com>
2020-09-30 11:14:35 -0500

    Initial commit. completed first task.

---
+++ Circle.cs
@@ -0,0 +1,7 @@
+namespace Studio1
+{
+    public class Circle
+    {
+        
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Studio1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Set input and convert to double
+            Console.WriteLine("Enter a radius:");
+            string radiusInput = Console.ReadLine();
+            double radius = double.Parse(radiusInput);
+            double areaRaw = Math.PI + radius * radius;
+            double area = Math.Round(areaRaw, 3);
+            Console.WriteLine($"The area of a circle with a radius of {radius} is {area}");
+            // Using the same radius, calculate the circumference (2*pi*r) and diameter of the circle (2*r).
+            double circumference = 2 * Math.PI * radius;
+            Console.WriteLine($"The circumference of a circle with a radius of {radius} is {circumference}");
+            Console.WriteLine("What is the miles per gallon of your car:");
+            string mphInput = Console.ReadLine();
+            double mph = double.Parse(mphInput);
+        }
+    }
+}
